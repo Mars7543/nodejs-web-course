384bb750ad5ff1b89b7430296c6ae09ff4c4e8bd
Mars7543 <marstech@hotmail.com>
2017-05-03 11:45:13 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+   return new Date().getFullYear();
+});
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+   var now = new Date().toString();
+   var log = `${now + ' ' + req.method + ' ' + req.url}`;
+
+   console.log(log);
+
+   fs.appendFile('server.log', log + '\n', (err) => {
+      if (err) console.log(err);
+   });
+
+   next();
+});
+
+app.use((req, res, next) => {
+   var x = Math.ceil(Math.random() * 10);
+
+   if (x > 7) res.render('maintenance');
+
+   next();
+});
+
+app.get('/', (req, res) => {
+   res.render('home', {
+      title : 'Homepage',
+      msg   : 'Welcome to this epic page!'
+   });
+});
+
+app.get('/about', (req, res) => {
+   res.render('about', {
+      pageTitle: 'About'
+   });
+});
+
+app.listen(3000, () => {
+   console.log("Serving app on port 3000...");
+});
